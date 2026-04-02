f56562e9096694e319be13d8c5c17948e590c421
alexteck01 <alexdepa9@gmail.com>
2026-04-02 18:46:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def qrcon():
     id_oggetto = request.args.get("id")
 
     storico = carica_da_supabase(id_oggetto)
-     note_correnti = storico.get("note", "")
+    note_correnti = storico.get("note", "")
 
     return render_template(
         "qrcon.html",
