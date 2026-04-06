0792df90bea255581bc50d4b763e242bf59dfbc8
alexteck01 <alexdepa9@gmail.com>
2026-04-06 22:17:17 +0200

    Update server.py

--- server.py
+++ server.py
@@ -136,7 +136,7 @@ def qrcon():
 
     storico_testo = "<br>".join([f"{e[0]} – {e[1]}" for e in eventi])
     
-return f"""
+    return f"""
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/static/style.css">
 
