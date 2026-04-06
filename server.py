cea65842f51e1a823b466cc1fa1fa94fe1a42676
alexteck01 <alexdepa9@gmail.com>
2026-04-06 19:23:49 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from datetime import datetime
 import pytz
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="qrcod")
 
 # ---------------------------------------------------------
 # CONFIGURAZIONE SUPABASE
