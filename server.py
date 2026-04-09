38ce4d905d6ba7993d69bf8fa94d27bd86ca0702
alexteck01 <alexdepa9@gmail.com>
2026-04-09 19:38:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -170,6 +170,31 @@ def qrcon():
     note_correnti = storico["note"]
 
     storico_testo = "<br>".join([f"{e[0]} – {e[1]}" for e in eventi])
+        return f"""
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/static/style.css">
+
+<div class="container">
+    <div class="box">
+        <div class="title">Storico</div>
+        {storico_testo}
+    </div>
+</div>
+
+<form action="/salva_note" method="POST">
+    <input type="hidden" name="id" value="{id_oggetto}">
+    <div class="container">
+        <div class="box">
+            <div class="title">Segnalazioni</div>
+            <textarea name="note" rows="8">{note_correnti}</textarea>
+        </div>
+    </div>
+    <div class="container">
+        <button type="submit">Salva</button>
+    </div>
+</form>
+"""
+
 
 # ---------------------------------------------------------
 # AVVIO SERVER
