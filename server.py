9e8a587b00661fe25364d6866af0ff0fe90cc101
alexteck01 <alexdepa9@gmail.com>
2026-04-20 20:11:59 +0200

    Update server.py

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def scan():
 
     return render_template_string(f"""
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/static/style.css">
+        <link rel="stylesheet" href="/static.style.css">
         
         <div class="container">
             <div class="box">
